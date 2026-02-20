f926c7f971b6233fbe22552b040a9c0c04a85d7e
Lorenzo Rodigliano <rodiglianolorenzo@gmail.com>
2026-02-20 15:16:49 +0100

    make the 1 task whit forEach

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -1,10 +1,8 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
 
-for (let i = 0; i < names.length; i++) {
-    const name = names[i];
+names.forEach(function(name) {
     console.log(name);
-    
-}   
+});
     
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
