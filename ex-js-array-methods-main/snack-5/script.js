b2ae64a0bb59b5d2359bd73b03bdebadf58084ca
Lorenzo Rodigliano <rodiglianolorenzo@gmail.com>
2026-02-20 13:57:42 +0100

    complete task 5

--- ex-js-array-methods-main/snack-5/script.js
+++ ex-js-array-methods-main/snack-5/script.js
@@ -1,4 +1,13 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
+
+const evenNumbs = nums.filter(numb => {
+    if(numb % 2 === 0){
+        return true
+    }
+        return false
+})
+console.log(evenNumbs);
+
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
