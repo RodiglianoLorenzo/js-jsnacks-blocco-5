0a46e3e04853b7dbb5d4eccd51193c254f38f4f9
Lorenzo Rodigliano <rodiglianolorenzo@gmail.com>
2026-02-20 13:06:57 +0100

    first commit

---
+++ ex-js-array-methods-main/snack-1/script.js
@@ -0,0 +1,10 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+for (let i = 0; i < names.length; i++) {
+    const name = names[i];
+    console.log(name);
+    
+}   
+    
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
